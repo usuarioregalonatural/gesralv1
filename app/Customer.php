af98fa88fae2535d2db95d8d02e003674e48eb73
Victor <webmaster@regalonatural.com>
2018-12-27 23:34:33 +0100

    Finalizada busqueda en listado Clientes

--- app/Customer.php
+++ app/Customer.php
@@ -8,4 +8,11 @@ class Customer extends Model
 {
     protected $fillable =['cod_cliente','nombre','direccion', 'telefono','email','poblacion','cod_postal', 'provincia','pais'];
 
+    public function scopeName($query, $nombre){
+        if($nombre)
+            return $query->orWhere('nombre', 'LIKE',"%$nombre%");
+    }
+
+
+
 }
